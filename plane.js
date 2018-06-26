3f378fd245c848a9cb8073f32422614e1df5e640
Luke Lowrey <luke.lowrey@achiive.co.uk>
2018-06-26 13:55:53 +0100

    Made the styles prettier. Elements are loaded into an array rather than hard coded from planes.

--- plane.js
+++ plane.js
@@ -1,7 +1,45 @@
-firstobj = {x:1, y:2, z:3}
-secondobj = {x:1, y:2, z:1}
+firstobj = {x: 1, y: 2, z: 3}
+secondobj = {x: 1, y: 2, z: 1}
 
-$(function() {
-    $('#canvas').append("<div class='plane' id='YYY123'>YYY123<br/>LHR - GOT</div>")
-    $('#YYY123').css({position: 'absolute', width: '100px', height: '40px', top: '100px', left: '50px'})
+$(function () {
+
+    var planes = [];
+
+
+    var plane = {
+        flightnumber: "YYY123",
+        operator: "PinguAir",
+        from: "London",
+        from_code: "LHR",
+        to: "Amsterdam",
+        to_code: "AMS",
+        aircraft_type: "A320",
+        top: "100px", // These will be dynamically updated
+        left: "100px"
+    };
+
+    var template = `
+        <div class="plane" id="${plane.flightnumber}">
+            <div>
+            <div class="number">${plane.flightnumber}</div><div class="operator">${plane.operator}</div>
+            </div>
+            <div>
+                <div class="from"><div class="code">${plane.from_code}</div>${plane.from}</div><div class="icon"><i class="fas fa-2x fa-plane"></i></div><div class="to"><div class="code">${plane.to_code}</div>${plane.to}</div>
+            </div>
+            <hr/>
+            <div><div class="aircraft">${plane.aircraft_type}</div></div>
+        </div>
+    `;
+
+    planes.push(plane);
+    plane.element = $(template).appendTo('#canvas');
+
+    updatePlanePositions();
+
+    function updatePlanePositions(){
+        planes.forEach((plane) => {
+            plane.element.css({top: plane.top, left: plane.left})
+        });
+    }
+
 })
