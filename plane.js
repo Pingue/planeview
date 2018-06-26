0956d8cd40f4ae3da7b7998f5ca31f5c91304dda
Mike Frost <Michael.Frost@rackspace.co.uk>
2018-06-26 12:54:45 +0100

    Binned canvas, use css positioning instead

--- plane.js
+++ plane.js
@@ -1,18 +1,7 @@
-alert("planes")
-main()
-function main()
-{
-    firstobj = {x:1, y:2, z:3}
-    secondobj = {x:1, y:2, z:1}
+firstobj = {x:1, y:2, z:3}
+secondobj = {x:1, y:2, z:1}
 
-    var c = document.getElementById("canvas");
-    var gl = c.getContext("webgl");
-
-    if (!gl) {
-        alert("Unable to initialize WebGL. Your browser or machine may not support it.");
-        return;
-    }
-    gl.viewport(0, 0, canvas.width, canvas.height);
-    gl.clearColor(0, 0.5, 0, 1);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-}
+$(function() {
+    $('#canvas').append("<div class='plane' id='YYY123'>YYY123<br/>LHR - GOT</div>")
+    $('#YYY123').css({position: 'absolute', width: '100px', height: '40px', top: '100px', left: '50px'})
+})
