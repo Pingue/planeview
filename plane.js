ff6c50ab637a824c9b5de2408a36576f7630d9c2
Mike Frost <Michael.Frost@rackspace.co.uk>
2018-06-26 13:48:10 +0100

    Failed attempt at maths

--- plane.js
+++ plane.js
@@ -15,6 +15,9 @@ function getPlaneData()
     var planes = [
         {name: "YYY111", lat: "100", long: "200", alt: "3", start: "LHR", end: "GOT"},
         {name: "YYY222", lat: "400", long: "500", alt: "6", start: "LGW", end: "LHR"},
+        {name: "YYY333", lat: "4", long: "500", alt: "6", start: "LGW", end: "LHR"},
+        {name: "YYY444", lat: "0", long: "0", alt: "6", start: "LGW", end: "LHR"},
+        {name: "YYY555", lat: "0", long: "0", alt: "100", start: "LGW", end: "LHR"},
     ]
     return planes
 }
@@ -26,10 +29,15 @@ function clearPlanes()
 
 function drawPlanes(planes)
 {
+    browserWidth = $( window ).width();
+    browserHeight = $( window ).height();
     for (var plane in planes)
     {
-        var x = planes[plane]['lat'] // (not really)
-        var y = planes[plane]['long'] // (not really)
+        // Start by subtracting my location from the plane location to get a relative location
+        var x = (browserWidth / 2) - (browserWidth / 2) * planes[plane]['lat']/planes[plane]['alt']
+        var y = (browserHeight / 2) - (browserHeight / 2) * planes[plane]['long']/planes[plane]['alt']
+        console.info(x)
+        console.info(y)
 
         $('#canvas').append("<div class='plane' id='"+planes[plane]['name']+"'>"+planes[plane]['name']+"<br/>"+planes[plane]['start']+" - "+planes[plane]['end']+"</div>")
         $('#'+planes[plane]['name']).css({position: 'absolute', width: '100px', height: '40px', top: x, left: y})
